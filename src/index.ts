cd0c20d1aea93b413fe03614779eebb4749dc6e3
Le Xu <le.xu@rea-group.com>
2023-07-13 15:48:07 +0800

    Fix: jest hangs when running yarn test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import express, {Request,Response} from 'express';
-export let app = express();
-
-app.listen('3000',() => {
-    console.log('welcome to the dev-ops world')
-})
+export const app = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.set('Content-Type','text/plain');
-    res.send("" +
+    res.status(200).send("" +
         " ____  _______     _____  ____  ____     ____ ___ ____  _     \n\n" +
         "|  _ \\| ____\\ \\   / / _ \\|  _ \\/ ___|   / ___|_ _|  _ \\| |    \n\n" +
         "| | | |  _|  \\ \\ / / | | | |_) \\___ \\  | |  _ | || |_) | |    \n\n" +
         "| |_| | |___  \\ V /| |_| |  __/ ___) | | |_| || ||  _ <| |___ \n\n" +
         "|____/|_____|  \\_/  \\___/|_|   |____/   \\____|___|_| \\_\\_____|\n\n");
-    res.send();
 });
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen('3000', () => {
+  console.log('welcome to the dev-ops world')
+})
